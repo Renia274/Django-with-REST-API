c3782caa9b4ceb179eb5f6b92134bbd72b527e97
Renia Dioleti <reniadiol3@gmail.com>
2023-06-09 15:49:40 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,10 +2,16 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import CategoryViewSet, ProductViewSet
 
+# Create a router object
 router = routers.DefaultRouter()
+
+# Register the CategoryViewSet with the router for the 'categories' URL pattern
 router.register(r'categories', CategoryViewSet)
+
+# Register the ProductViewSet with the router for the 'products' URL pattern
 router.register(r'products', ProductViewSet)
 
+# Define the URL patterns
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),  # Include the router URLs in the root URLconf
 ]
