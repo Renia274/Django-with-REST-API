4b0e9ab3284344ca0d4556de1cec09569e3347c8
Renia Dioleti <reniadiol3@gmail.com>
2023-06-09 15:55:28 +0300

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,7 +3,12 @@ from django.urls import include, path
 from myapp.views import home_view
 
 urlpatterns = [
+    # Home page URL mapped to home_view function
     path('', home_view, name='home'),
+    
+    # Admin site URL
     path('admin/', admin.site.urls),
+    
+    # API URL patterns included from 'myapp.urls' module
     path('api/', include('myapp.urls')),
 ]
