4972c704aab84b3d059a22f7ca4da1bead8aff71
Renia Dioleti <reniadiol3@gmail.com>
2023-06-09 15:42:10 +0300

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 
-
+# Category model
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)  # Name field for the category
 
     class Meta:
-        db_table = 'categories_category'
+        db_table = 'categories_category'  # Database table name for the Category model
 
     def __str__(self):
-        return self.name
+        return self.name  # String representation of the Category model
 
 
+# Product model
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)  # Name field for the product
+    description = models.TextField()  # Description field for the product
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # Foreign key relationship to Category model
 
     class Meta:
-        app_label = 'products'
+        app_label = 'products'  # App label for the Product model
 
     def __str__(self):
-        return self.name
+        return self.name  # String representation of the Product model
+
